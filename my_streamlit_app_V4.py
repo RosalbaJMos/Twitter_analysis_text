189d9febb2be82d5abf86a3ce3d6fc62c98164ff
rjuarez <rjuarezmos@gmail.com>
2022-02-03 16:41:44 +0100

    commit

--- my_streamlit_app_V4.py
+++ my_streamlit_app_V4.py
@@ -18,6 +18,8 @@ from gensim.models import CoherenceModel
 import matplotlib.pyplot as plt
 
 # NLTK Stop words
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'])
